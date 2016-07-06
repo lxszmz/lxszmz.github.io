b39f1c37f5d8c38ab898efe335cce5a21f987f5d
lixiaosong <1531503999@qq.com>
2016-07-06 11:09:19 +0800

    H%5

--- page/js/H5.js
+++ page/js/H5.js
@@ -1,47 +1,82 @@
 /* 内容管理对象 */
-var H5 = function(name ,cfg){
+var H5 = function(){
     var id = ("h5_"+Math.random()).replace(".","_");
     this.el = $('<div id="'+id+'" >').hide();
     $("body").append(this.el);
     var pageList=[];
-    
+
+
+    /**
+     * 增加页面
+     * return this 是为了使用链式调用
+     * pageList 是为了添加组件时知道向哪一个页面添加
+     * @param className
+     * @returns {H5}
+     */
     this.addPage = function (className){
         var id = ("h5_page_"+Math.random()).replace(".","_");
-        var page = $('<div id="'+id+'" class= "section page">');
+        var page = $('<div id="'+id+'" class= "section page ">');
         if(!className){
             page.addClass(className);
         }
-        
         this.el.append(page);
         pageList.push(page)
-        
-        
         return this;
-    }
-    
+    };
+
+    /**
+     * 页面增加组件
+     *
+     * @param name
+     * @param cfg
+     * @returns {H5}
+     */
       this.addComponent= function (name,cfg){
           var cfg = cfg||{};
-          $.extend({
+
+          /**
+           * 如果没有定义组件的类型，那么默认是base类型
+           * 使用jQuery中的扩展功能
+           */
+          cfg= $.extend({
               type:"base"
-          },cfg)
+          },cfg);
           
           var component;
+          /**
+           * 取出最近添加的页面
+           */
           var page= pageList.slice(-1)[0];
+
+          /**
+           * 根据类型调用不同的组件
+           */
           switch (cfg.type) {
               case 'base':
-                 component= new h5Component(name,cfg);
+                 component= new H5ComponentBase(name,cfg);
                  page.append(component);
                   break;
               
               default:
                   // code
-          }
+                  break;
+          };
         
         return this;
     }
-    
+
+    /**
+     * 这个是为了等待页面加载完全在将这些组件显示出来
+     */
     this.loader= function(){
-        this.el.fullpage();
+        this.el.fullpage({
+            onLeave:function(){
+                $(this).find(".h5_component").trigger("leave");
+            },
+            afterLoad:function(){
+                $(this).find(".h5_component").trigger("load");
+            }
+        });
         this.el.show();
     }
     
--- page/js/H5ComponentPoint.js
+++ page/js/H5ComponentPoint.js
@@ -1,2 +1,22 @@
-/* 散点图表组件对象 */
+/* 基本图文组件对象 */
 
+var H5ComponentPoint = function(name,cfg){
+    var baseCompoent = new H5ComponentBase(name,cfg);
+    var base = cfg.data[0][1];
+
+    $.each(cfg.data,function(index,item){
+        console.log(item[0]);
+        var point = $('<div class="point">'+item[0]+'</div>')
+        var per = (item[1]/base*100)+"%";
+        point.width(per).height(per);
+        if(item[2]){
+            point.css("background-color",item[2]);
+        }
+        if(item[3]!==undefined&&item[4]!=undefined){
+            point.css("left",item[3]).css("top",item[4]);
+        }
+        baseCompoent.append(point);
+    });
+    return baseCompoent;
+
+}
